47fd1a6ba4c7b144d4be3b93a390b48b1bd7c3df
Matjaž Mav <matjaz.mav@gmail.com>
2020-11-27 23:54:40 +0100

    ex

--- src/main/resources/template/Main.java
+++ src/main/resources/template/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println(0);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final int T = _in.nextInt();
         for (int i = 0; i < T; i++) {
             solve();
