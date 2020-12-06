69a9bade2e0004bf7f744d409a49a08ff62795a8
Matjaž Mav <matjaz.mav@gmail.com>
2020-12-06 13:36:50 +0100

    ...

--- src/main/java/APARTS/Main.java
+++ src/main/java/APARTS/Main.java
@@ -3,8 +3,6 @@ package APARTS;
 import java.io.*;
 import java.util.*;
 
-// **NOTE**: C++ solution is needed!
-
 public class Main {
     private static final Scanner _in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 
@@ -37,37 +35,6 @@ public class Main {
             System.out.println();
         }
     }
-
-//    private static void solve() {
-//        final int N = _in.nextInt();
-//        final int M = _in.nextInt();
-//        final int[][] A = new int[N][M];
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < M; j++) {
-//                A[i][j] =  _in.nextInt();
-//            }
-//        }
-//
-//        List<Integer> upperWindows = new ArrayList<Integer>(3);
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < M; j++) {
-//                if(i > 0) {
-//                    upperWindows.add(A[i-1][j]);
-//                    if(j > 0) upperWindows.add(A[i-1][j-1]);
-//                    if(j < M - 1) upperWindows.add(A[i-1][j+1]);
-//                }
-//                int max = upperWindows.size() > 0 ? Collections.max(upperWindows) : 0;
-//                if (A[i][j] > max) {
-//                    System.out.print("1");
-//                } else {
-//                    A[i][j] = max;
-//                    System.out.print("0");
-//                }
-//                upperWindows.clear();
-//            }
-//            System.out.println();
-//        }
-//    }
 
     public static void main(String[] args) throws Exception {
         final int T = _in.nextInt();
